914f7ea5282ce0ee81d81e8fa50045e03760a8dd
Elias Eskelinen <elias.eskelinen@pm.me>
2021-05-10 23:47:31 +0300

    Settings are now saved to localStorage

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,10 @@
+<svelte:head>
+	<!-- title, might be customizable later -->
+	<title> New Tab </title>
+	<!-- a hack to make the favicon blank with as little effort as possible -->
+	<link rel="icon" href="data:;base64,iVBORw0KGgo=">
+</svelte:head>
+
 <script>
 	import Settings from "./Settings.svelte";
 	let settings_visible = false;
@@ -7,15 +14,33 @@
 		settings_visible = !settings_visible;
 		settings_display = settings_visible?'inherit':'none';
 	}
+	let date = new Date();
+	$: time = date.toLocaleTimeString().replaceAll(".", ":");
+		const interval = setInterval(() => {
+			date = new Date();
+		}, 1000);
+	let greeting = "Good morning,";
+	var curHr = date.getHours();
+		if (curHr < 12) {
+			greeting = "Good morning,";
+		} else if (curHr < 18) {
+			greeting = "Good afternoon,";
+		} else if (curHr < 21) {
+			greeting = "Good afternoon,";
+		} else {
+			greeting = "Good night,";
+		}
 </script>
 
 <div class="content" style="display:{settings_display}; background: black; padding: 0; margin: 0;">
 <Settings bind:settings={settings} />
 </div>
-<main style="--bg-start: {settings.background_gradient_start};--bg-end: {settings.background_gradient_end};--bg-rot: {settings.background_rot}deg;--bg-handle1: {settings.background_handle1}%;">
-	<p class="logo"> Homelaunch <button on:click={toggle_settings}> Settings </button> </p>
+<main style="--bg-start: {settings.background_gradient_start};--bg-end: {settings.background_gradient_end};--bg-rot: {settings.background_rot}deg;--bg-handle1: {settings.background_handle1}%;--bg-handle2: {settings.background_handle2}%;">
+	<p class="logo"> Homelaunch <button on:click={toggle_settings} class="settingsbtn"> Settings </button> </p>
 	<div class="content">
-		<h1> Good morning, {settings.name} </h1>
+		<h1> {greeting} {settings.name} </h1>
+		<h1> The time is {time} </h1>
+		<input type="text" id="query" name="query" autofocus><br>
 	</div>
 </main>
 
@@ -28,11 +53,11 @@
 	main {
 		height: 100%;
 		background: rgb(0,0,0);
-		background: linear-gradient(var(--bg-rot), var(--bg-start) var(--bg-handle1), var(--bg-end) 100%); 
-		color: #DDDDDD;
+		background: linear-gradient(var(--bg-rot), var(--bg-start) var(--bg-handle1), var(--bg-end) var(--bg-handle2)); 
+		color: #FFFFFF;
 	}
 	.logo {
-		color: #DDDDDD;
+		color: #BBBBBB;
 		font-family: Comic Sans MS;
 		margin: 0;
 	}
@@ -40,6 +65,31 @@
 		text-align: center;
 		padding: 1em;
 		margin: 0 auto;
+	}
+	#query {
+		border: none;
+		border-radius: 2em;
+		width: 20em;
+	    padding-left: 1em;
+    	padding-right: 1em;
+    	transition: outline .1s;
+	}
+	#query:focus, query:focus{
+	    outline: .24em solid gray;
+	}
+	h1{
+		
+		font-family: Cantarell, "Segoe UI", Ubuntu, Roboto;
+		font-weight: 300 !important;
+	}
+	.settingsbtn{
+		background: none;
+		color: #BBBBBB;
+		border: 1px solid rgba(1, 1, 1, 0);
+		text-decoration: underline;
+	}
+	.settingsbtn:hover {
+		border: 1px solid gray;
 	}
 	p {
 	}
--- src/Settings.svelte
+++ src/Settings.svelte
@@ -1,41 +1,71 @@
 <script>
-	import { writable } from "svelte/store";
-	const storedTheme = localStorage.getItem("theme");
-	export const theme = writable(storedTheme);
-	theme.subscribe(value => {
-	    localStorage.setItem("theme", value);
-	});
-	let name = "Elias";
+	let name = "Name Here";
 	let background_gradient_start = "#1c1b21";
-	let background_gradient_end = "rgba(132,75,218,1)";
+	let background_gradient_end = "#3A2856"; //Originally rgba(132,75,218,1)
 	let background_rot = "180";
-	let background_handle1 = "5";
+	let background_handle1 = "10";
+	let background_handle2 = "100";
 	export let settings;
 	settings = {
+		"valid" : true,
 		"name" : name,
 		"background_gradient_start" : background_gradient_start,
 		"background_gradient_end" : background_gradient_end,
 		"background_rot" : background_rot,
-		"background_handle1" : background_handle1
+		"background_handle1" : background_handle1,
+		"background_handle2" : background_handle2
+	}
+	const stored = localStorage.getItem('settings');
+	let error = "";
+	if (stored != null){
+		try{
+			let sets = JSON.parse(stored);
+			console.log(sets);
+			if(sets.valid === true){
+				settings = sets;
+			}
+		}
+		catch{
+			error = "error: couldn't parse json: '" + stored + "'";
+			console.log(error);
+		}
+	}
+	function save(){
+		localStorage.setItem('settings', JSON.stringify(settings));
+	}
+	let code = "";
+	function showCode(){
+		code = JSON.stringify(settings);
+	}
+	function clear(){
+		localStorage.clear();
 	}
 </script>
 
-<main style="--bg-start: {settings.background_gradient_start};--bg-end: {settings.background_gradient_end};--bg-rot: {settings.background_rot}deg;--bg-handle1: {settings.background_handle1}%;">
+<main style="--bg-start: {settings.background_gradient_start};--bg-end: {settings.background_gradient_end};--bg-rot: {settings.background_rot}deg;--bg-handle1: {settings.background_handle1}%;--bg-handle2: {settings.background_handle2}%;">
 <div class="settings" id="settings">
 	<h1> Settings </h1>
 	<div>
+		<label for="name"> Name <input type=text id="name" bind:value={settings.name}> </label>
 		<label for="gradient-rot">Gradient rotation
 			<input type=number bind:value={settings.background_rot} min=0 max=360> 
 			<input type=range bind:value={settings.background_rot} min=0 max=360 id="gradient-rot"> 
 		</label>
-		<label for="gradient-per">Gradient percentage
+		<label for="gradient-per">Gradient percentage 1
 			<input type=number bind:value={settings.background_handle1} min=0 max=100> 
 			<input type=range bind:value={settings.background_handle1} min=0 max=100 id="gradient-per"> 
+		</label>
+		<label for="gradient-per2">Gradient percentage 2
+			<input type=number bind:value={settings.background_handle2} min=0 max=200> 
+			<input type=range bind:value={settings.background_handle2} min=0 max=200 id="gradient-per2"> 
 		</label>
 		<br />
 		<label for="gradient-1"> Gradient start color <input type="color" id="gradient-1" bind:value={settings.background_gradient_start}></label>
 		<label for="gradient-2"> Gradient end color <input type="color" id="gradient-2" bind:value={settings.background_gradient_end}></label>
 	</div>
+	<h3> {error} </h3>
+	<button on:click={save} > Save </button> <button on:click={showCode} > Show JSON </button> <button on:click={clear} > Reset </button>
+	<p> {code} </p>
 </div>
 </main>
 
@@ -45,8 +75,10 @@
 		color: white;
 	}
 	h1{
-		
 		margin: 0;
 		padding: 0;
+	}
+	p{
+		margin: 0;
 	}
 </style>
